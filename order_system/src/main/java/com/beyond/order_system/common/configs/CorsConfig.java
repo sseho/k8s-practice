5373790cd1be85a6ae20e5276443449b2ff61543
sseho <kindseho@naver.com>
2024-08-30 17:28:55 +0900

    fix

--- order_system/src/main/java/com/beyond/order_system/common/configs/CorsConfig.java
+++ order_system/src/main/java/com/beyond/order_system/common/configs/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**") // 접근할 수 있는 uri 허용 범위
-                .allowedOrigins("http://localhost:3000") // 허용 url명시
+                .allowedOrigins("https://www.ssebao.shop") // 허용 url명시
                 .allowedMethods("*") // 모든 메서드에 대해 허용
                 .allowedHeaders("*")
                 .allowCredentials(true);

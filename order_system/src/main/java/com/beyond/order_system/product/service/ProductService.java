b570ff1cbe67b76ad7538a35abbddd7e6ea55b94
sseho <kindseho@naver.com>
2024-08-30 17:53:07 +0900

    fix

--- order_system/src/main/java/com/beyond/order_system/product/service/ProductService.java
+++ order_system/src/main/java/com/beyond/order_system/product/service/ProductService.java
@@ -80,7 +80,7 @@ public class ProductService {
             product = productRepository.save(dto.toEntity());
             byte[] bytes = image.getBytes();
             String fileName = product.getId() + "_" + image.getOriginalFilename();
-            Path path = Paths.get("C:/Users/PlayData/Desktop/tmp/", fileName);
+            Path path = Paths.get("/tmp/", fileName);
 //            local pc에 임시 저장
             Files.write(path, bytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
 
